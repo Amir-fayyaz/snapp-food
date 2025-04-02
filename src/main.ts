ad23083de16b6b3041290e0048dab1193950f443
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-02 14:54:34 -0700

    For initialize project structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+// internal modules => user.module.ts , ...
+// @Global() => for AppModule providers
+// configs => typeOrmModule.forRoot() , staticServe.forRoot() , ...

fab0d67b66e363f1249084b7f3158a7ed40f8514
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-05 03:44:16 -0700

    add requirment providers into category.admin.module.ts

--- src/modules/category/admin/category.admin.module.ts
+++ src/modules/category/admin/category.admin.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CategoryAdminController } from './category.admin.controller';
+import { CategoryAdminService } from './category.admin.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryEntity } from '../entities/category.entity';
+import { S3Service } from 'src/modules/s3/s3.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([CategoryEntity])],
+  controllers: [CategoryAdminController],
+  providers: [CategoryAdminService, S3Service],
 })
 export class CategoryAdminModule {}

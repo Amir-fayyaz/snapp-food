6232284235a8e459e3ed321f819d7c1f840a5dd5
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-05 11:51:19 -0700

    add validation pipe to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { InitializeSwagger } from './common/configs/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   InitializeSwagger(app);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   const { PORT = 3000 } = process.env;
   await app.listen(PORT, () => {
     console.log(`swagger run on http://localhost:${PORT}/docs`);

6848b061199727a8791c707e2c29616fb5b140a8
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-05 11:53:48 -0700

    add s3Module to project

--- src/modules/global/internal-modules.ts
+++ src/modules/global/internal-modules.ts
@@ -1,3 +1,4 @@
 import { CategoryModule } from '../category/category.module';
+import { S3Module } from '../s3/s3.module';
 
-export const InternalModules = [CategoryModule];
+export const InternalModules = [CategoryModule, S3Module];

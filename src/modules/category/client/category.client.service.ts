323303c9f403724a9548cebfe797f4a9fc27fedc
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-05 23:52:31 -0700

    add getCategories service into category.client.service.ts

--- src/modules/category/client/category.client.service.ts
+++ src/modules/category/client/category.client.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoryEntity } from '../entities/category.entity';
 import { Repository } from 'typeorm';
+import { Pagination } from 'src/common/utility/pagination.utilty';
+import { PaginationType } from 'src/common/types/pagination.type';
 
 @Injectable()
 export class CategoryAppService {
@@ -21,6 +23,22 @@ export class CategoryAppService {
       throw new NotFoundException('There is no category with this id');
 
     return category;
+  }
+
+  public async getCategories(data: PaginationType) {
+    const pagination = Pagination({ page: data.page, take: data.take });
+
+    const categories = await this.Category_Repository.find({
+      relations: ['parent'],
+      take: pagination.take,
+      skip: pagination.skip,
+    });
+
+    return {
+      page: data.page,
+      categories,
+      count: categories.length,
+    };
   }
   //For exports
 }

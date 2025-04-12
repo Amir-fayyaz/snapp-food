8a1cc86b5c989d3f6df51711bd7c2cc8d8b87f25
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-12 10:04:32 -0700

    add dependencies to auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthAppModule } from './client/auth.client.module';
 import { AuthAdminModule } from './admin/auth.admin.module';
+import { JwtModule } from '@nestjs/jwt';
+import { Jwtconfig } from 'src/common/configs/jwt.config';
 
 @Module({
-  imports: [AuthAppModule, AuthAdminModule],
+  imports: [AuthAppModule, AuthAdminModule, JwtModule.register(Jwtconfig)],
   controllers: [],
   providers: [],
 })

9ecebaffff3f1ea7456ca2111dff2244e53507b5
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-12 13:15:39 -0700

    add exports methods for use in another services

---
+++ src/modules/users/client/user.client.service.ts
@@ -0,0 +1,28 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserAppService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly User_Repository: Repository<UserEntity>,
+  ) {}
+
+  //For exports
+  public async CreateUser(mobile: string) {
+    const user = await this.User_Repository.findOneBy({ mobile });
+
+    if (user)
+      throw new ConflictException('There is another acount with this mobile');
+
+    const newUser = this.User_Repository.create({ mobile });
+
+    return await this.User_Repository.save(newUser);
+  }
+
+  public async findUserByMobile(mobile: string) {
+    return await this.User_Repository.findOne({ where: { mobile } });
+  }
+}

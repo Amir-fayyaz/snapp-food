c3f40ef8617dfb68d5eb2a44fac3b13503a420f6
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-13 12:47:17 -0700

    add JwtConfigs to auth.client.service.ts

--- src/modules/auth/client/auth.client.module.ts
+++ src/modules/auth/client/auth.client.module.ts
@@ -3,10 +3,17 @@ import { AuthAppController } from './auth.client.controller';
 import { AuthAppService } from './auth.client.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OtpEntity } from '../entities/otp.entity';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { UserAppService } from 'src/modules/users/client/user.client.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { Jwtconfig } from 'src/common/configs/jwt.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OtpEntity])],
+  imports: [
+    TypeOrmModule.forFeature([OtpEntity, UserEntity]),
+    JwtModule.register(Jwtconfig),
+  ],
   controllers: [AuthAppController],
-  providers: [AuthAppService],
+  providers: [AuthAppService, UserAppService, JwtService],
 })
 export class AuthAppModule {}

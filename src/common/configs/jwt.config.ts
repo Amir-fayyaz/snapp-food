8797cfd80db0dc2ed662f61b11a3984e4c83fa35
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-13 12:43:43 -0700

    update JwtConfig.ts

--- src/common/configs/jwt.config.ts
+++ src/common/configs/jwt.config.ts
@@ -1,9 +1,12 @@
-import { JwtModuleAsyncOptions, JwtModuleOptions } from '@nestjs/jwt';
+import { JwtModuleAsyncOptions } from '@nestjs/jwt';
 import { config } from 'dotenv';
 
 config();
 
-export const Jwtconfig: JwtModuleOptions = {
-  secret: process.env.JWT_SECRET,
-  signOptions: { expiresIn: process.env.JWT_EXPIRE },
+export const Jwtconfig: JwtModuleAsyncOptions = {
+  useFactory: () => ({
+    secret: process.env.JWT_SECRET,
+
+    signOptions: { expiresIn: process.env.JWT_EXPIRE },
+  }),
 };

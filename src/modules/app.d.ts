8ed7d4b4cdeafb7878f3cc7c29aa90e517236e2e
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-14 10:44:25 -0700

    update app.d.ts

--- src/modules/app.d.ts
+++ src/modules/app.d.ts
@@ -1,17 +1,3 @@
-//For env
-namespace NodeJs {
-  interface ProcessEnv {
-    //For App
-    PORT: number;
-
-    //For DB
-    DB_PORT: number;
-    DB_NAME: string;
-    DB_PASSWORD: string;
-    DB_USERNAME: string;
-    DB_HOST: string;
-  }
-}
 //For request
 declare global {
   namespace Express {

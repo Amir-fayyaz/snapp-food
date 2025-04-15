114160a4014d18255684dfed3a9037e0f6359b19
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-15 02:43:41 -0700

    add AuthSupplier to auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,15 @@ import { AuthAppModule } from './client/auth.client.module';
 import { AuthAdminModule } from './admin/auth.admin.module';
 import { JwtModule } from '@nestjs/jwt';
 import { Jwtconfig } from 'src/common/configs/jwt.config';
+import { AuthSupplierModule } from './supplier/auth.supplier.module';
 
 @Module({
-  imports: [AuthAppModule, AuthAdminModule, JwtModule.registerAsync(Jwtconfig)],
+  imports: [
+    AuthAppModule,
+    AuthAdminModule,
+    JwtModule.registerAsync(Jwtconfig),
+    AuthSupplierModule,
+  ],
   controllers: [],
   providers: [],
 })

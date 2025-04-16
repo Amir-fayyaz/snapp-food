e110ab256acd36526ce39989a2671741db02b78b
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-16 11:33:28 -0700

    add createSupplier method in supplier.service.ts

---
+++ src/modules/supplier/dto/create-supplier.dto.ts
@@ -0,0 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsPhoneNumber,
+  IsPositive,
+  IsString,
+  Length,
+} from 'class-validator';
+import { City } from 'src/modules/supplier/enums/city.enum';
+
+export class CreateSupplierDto {
+  @ApiProperty({ example: 'amir' })
+  @IsString()
+  @Length(3, 50)
+  manager_firstname: string;
+
+  @ApiProperty({ example: 'Fayyaz' })
+  @IsString()
+  @Length(3, 50)
+  manager_lastname: string;
+
+  @ApiProperty({ example: '09921810208' })
+  @IsPhoneNumber('IR')
+  manager_mobile: string;
+
+  @ApiProperty({ enum: City })
+  @IsEnum(City)
+  city: City;
+
+  @IsPositive()
+  @IsInt()
+  @ApiProperty({ example: 1 })
+  category_id: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  invite_code: string;
+}
---
+++ src/modules/supplier/supplier.service.ts
@@ -0,0 +1,63 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SupplierEntity } from './entities/supplier.entity';
+import { Repository } from 'typeorm';
+import { CreateSupplierDto } from './dto/create-supplier.dto';
+import { CategoryAppService } from '../category/client/category.client.service';
+
+@Injectable()
+export class SupplierService {
+  constructor(
+    @InjectRepository(SupplierEntity)
+    private readonly Supplier_Repository: Repository<SupplierEntity>,
+    private readonly categoryService: CategoryAppService,
+  ) {}
+
+  //public methods
+  public async createSupplier(data: CreateSupplierDto) {
+    //Check is unique phone-number or not
+    const supplier = await this.Supplier_Repository.findOne({
+      where: { manager_mobile: data.manager_mobile },
+    });
+
+    if (supplier)
+      throw new ConflictException(
+        'already There is supplier with this phone-number ',
+      );
+
+    const category = await this.categoryService.getCategoryById(
+      data.category_id,
+    );
+
+    if (data.invite_code) {
+      const agent = await this.Supplier_Repository.findOne({
+        where: { invite_code: data.invite_code },
+      });
+
+      if (!agent) throw new NotFoundException('Invalid invite-code');
+
+      const newSupplier = this.Supplier_Repository.create({
+        manager_firstname: data.manager_firstname,
+        manager_lastname: data.manager_lastname,
+        manager_mobile: data.manager_mobile,
+        category,
+        agent,
+      });
+
+      return await this.Supplier_Repository.save(newSupplier);
+    } else {
+      const newSupplier = this.Supplier_Repository.create({
+        manager_firstname: data.manager_firstname,
+        manager_lastname: data.manager_lastname,
+        manager_mobile: data.manager_mobile,
+        category,
+      });
+
+      return await this.Supplier_Repository.save(newSupplier);
+    }
+  }
+}

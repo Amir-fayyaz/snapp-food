a129bfe1d90fc5fb3e49741a1b2c44ec4ac40acf
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-19 12:32:06 -0700

    add login service in auth.supplier.controller.ts

--- src/modules/auth/supplier/auth.supplier.controller.ts
+++ src/modules/auth/supplier/auth.supplier.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthSupplierService } from './auth.supplier.service';
 import { SignUpDto } from './dto/signUp.dto';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { SignInDto } from './dto/signIn.dto';
 
 @Controller('api/v1/auth/supplier')
 @ApiTags('Auth-supplier')
@@ -13,5 +14,12 @@ export class AuthSupplierController {
   @ApiBody({ type: SignUpDto })
   async getOtp(@Body() data: SignUpDto) {
     return await this.AuthService.getOtp(data.mobile);
+  }
+
+  @Post('login')
+  @ApiOperation({ summary: 'For login/register to acount' })
+  @ApiBody({ type: SignInDto })
+  async login(@Body() data: SignInDto) {
+    return await this.AuthService.login(data);
   }
 }

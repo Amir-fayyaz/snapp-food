fd5f96077e8f52ca659e2f4207abee87d27b8c44
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-25 03:09:42 -0700

    add Role guard for better auth

--- src/modules/menu/supplier/controllers/menu-type.supplier.controller.ts
+++ src/modules/menu/supplier/controllers/menu-type.supplier.controller.ts
@@ -5,6 +5,7 @@ import { SupplierAuth } from 'src/common/decorators/auth.decorator';
 import { getUser } from 'src/common/decorators/getusers.decorator';
 import { SupplierEntity } from 'src/modules/supplier/entities/supplier.entity';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Role } from 'src/common/decorators/role.decorator';
 
 @Controller('api/v1/supplier/menu-type')
 @ApiTags('supplier menu-type')
@@ -15,10 +16,12 @@ export class MenuTypeSupplierController {
   @Post()
   @ApiOperation({ summary: 'For create new Menu-type' })
   @ApiBody({ type: CreateMenuTypeDto })
+  @Role(['supplier'])
   async createMenuType(
     @Body() data: CreateMenuTypeDto,
-    @getUser('supplier') supplier: SupplierEntity,
+    @getUser('supplier') supplier: any,
   ) {
+    console.log(supplier);
     return await this.MenuTypeService.createMenuType(data, supplier.id);
   }
 }

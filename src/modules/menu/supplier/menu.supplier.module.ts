be9fcf468c807502f7e904298b051e9cc8182cfc
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-27 04:51:40 -0700

    add dependencies for menu module

--- src/modules/menu/supplier/menu.supplier.module.ts
+++ src/modules/menu/supplier/menu.supplier.module.ts
@@ -8,17 +8,28 @@ import { SupplierEntity } from 'src/modules/supplier/entities/supplier.entity';
 import { CategoryAppService } from 'src/modules/category/client/category.client.service';
 import { CategoryEntity } from 'src/modules/category/entities/category.entity';
 import { JwtService } from '@nestjs/jwt';
+import { MenuSupplierController } from './controllers/menu.supplier.controller';
+import { MenuSupplierService } from './services/menu.supplier.service';
+import { MenuEntity } from '../entities/menu.entity';
+import { S3Service } from 'src/modules/s3/s3.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TypeEntity, SupplierEntity, CategoryEntity]),
+    TypeOrmModule.forFeature([
+      TypeEntity,
+      SupplierEntity,
+      CategoryEntity,
+      MenuEntity,
+    ]),
   ],
-  controllers: [MenuTypeSupplierController],
+  controllers: [MenuTypeSupplierController, MenuSupplierController],
   providers: [
+    MenuSupplierService,
     MenuTypeSupplierService,
     SupplierService,
     CategoryAppService,
     JwtService,
+    S3Service,
   ],
 })
 export class MenuSupplierModule {}

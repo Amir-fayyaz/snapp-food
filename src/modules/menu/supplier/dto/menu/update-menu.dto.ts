7a40520f612f619cb1c2731dc1080e11988cb3c5
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-29 01:08:45 -0700

    add update-menu service

---
+++ src/modules/menu/supplier/dto/menu/update-menu.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumberString, IsOptional, IsString, Length } from 'class-validator';
+
+export class UpdateMenuDto {
+  @ApiProperty()
+  @IsString()
+  @Length(3, 50)
+  name: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  image: Express.Multer.File;
+
+  @ApiProperty({ example: 19.5 })
+  @IsNumberString()
+  price: number;
+
+  @ApiProperty({ maximum: 100, minimum: 0, example: 10 })
+  @IsNumberString()
+  discount: number;
+
+  @ApiProperty()
+  @IsString()
+  @Length(3, 150)
+  description: string;
+
+  @ApiProperty({ minimum: 0, maximum: 5, example: 2, default: 0 })
+  @IsNumberString()
+  score: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumberString()
+  type_id: number;
+}
--- src/modules/menu/supplier/services/menu.supplier.service.ts
+++ src/modules/menu/supplier/services/menu.supplier.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { MenuEntity } from '../../entities/menu.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,6 +7,7 @@ import { SupplierService } from 'src/modules/supplier/supplier.service';
 import { CreateMenuDto } from '../dto/menu/create-menu.dto';
 import { S3Service } from 'src/modules/s3/s3.service';
 import { StorageFolderName } from 'src/common/enums/storage-folderNames.enum';
+import { UpdateMenuDto } from '../dto/menu/update-menu.dto';
 
 @Injectable()
 export class MenuSupplierService {
@@ -45,5 +46,72 @@ export class MenuSupplierService {
     });
 
     return await this.Menu_Repository.save(newFood);
+  }
+
+  public async updateFood(
+    data: UpdateMenuDto,
+    id: number,
+    supplier_id: number,
+  ) {
+    const { description, discount, image, name, price, score, type_id } = data;
+
+    const Food = await this.Menu_Repository.findOne({
+      where: { id, supplier: { id: supplier_id } },
+    });
+
+    if (!Food) throw new NotFoundException('Not found menu with this id');
+
+    //change of image
+    if (image) {
+      await this.S3Service.deleteFile(Food.image);
+      const { key } = await this.S3Service.uploadFile(
+        image,
+        StorageFolderName.Image,
+      );
+
+      if (type_id) {
+        const type = await this.MenuTypeService.getMenuTypeById(type_id);
+
+        await this.Menu_Repository.update(
+          { id },
+          {
+            description,
+            discount,
+            image: key,
+            name,
+            price,
+            score: score || 0,
+            type,
+          },
+        );
+
+        return { success: true };
+      } else {
+        await this.Menu_Repository.update(
+          { id },
+          { description, discount, image: key, name, price, score: score || 0 },
+        );
+
+        return { success: true };
+      }
+    } else {
+      if (type_id) {
+        const type = await this.MenuTypeService.getMenuTypeById(type_id);
+
+        await this.Menu_Repository.update(
+          { id },
+          { description, discount, name, price, score: score || 0, type },
+        );
+
+        return { success: true };
+      } else {
+        await this.Menu_Repository.update(
+          { id },
+          { description, discount, name, price, score: score || 0 },
+        );
+
+        return { success: true };
+      }
+    }
   }
 }

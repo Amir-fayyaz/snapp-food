29121c944524aa577d313af4d63e193460f62e29
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-29 01:08:56 -0700

    add update-menu api

--- src/modules/menu/supplier/controllers/menu.supplier.controller.ts
+++ src/modules/menu/supplier/controllers/menu.supplier.controller.ts
@@ -1,20 +1,24 @@
 import {
   Body,
   Controller,
+  Param,
+  ParseIntPipe,
   Post,
+  Put,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { MenuSupplierService } from '../services/menu.supplier.service';
 import { CreateMenuDto } from '../dto/menu/create-menu.dto';
-import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiParam } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { getUser } from 'src/common/decorators/getusers.decorator';
 import { ISupplier } from 'src/common/types/request-user.types';
 import { SupplierAuth } from 'src/common/decorators/auth.decorator';
+import { UpdateMenuDto } from '../dto/menu/update-menu.dto';
 
 @Controller('api/v1/supplier/menu')
-@SupplierAuth()
+// @SupplierAuth()
 export class MenuSupplierController {
   constructor(private readonly MenuService: MenuSupplierService) {}
 
@@ -47,5 +51,36 @@ export class MenuSupplierController {
     @getUser() supplier: ISupplier,
   ) {
     return await this.MenuService.createFood(data, supplier.supplier_id, image);
+  }
+
+  @Put(':id')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'required data',
+    type: 'multipart/form-data',
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        price: { type: 'number' },
+        discount: { type: 'number', default: 0 },
+        description: { type: 'string' },
+        score: { type: 'number', maximum: 5, minimum: 0, default: 0 },
+        type_id: { type: 'number', example: 2 },
+        image: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+      required: ['name', 'price', 'description', 'discount'],
+    },
+  })
+  @ApiParam({ name: 'id', description: 'Menu-id want to update' })
+  async updateMenu(
+    @Body() data: UpdateMenuDto,
+    @Param('id', ParseIntPipe) id: number,
+    @getUser() supplier: ISupplier,
+  ) {
+    return await this.MenuService.updateFood(data, id, 1);
   }
 }

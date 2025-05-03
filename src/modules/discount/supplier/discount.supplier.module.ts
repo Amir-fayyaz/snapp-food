db21e0ab582bfc8e134f45a37401be2d47f28930
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-03 10:07:03 -0700

    add dependencies to discount.supplier.module.ts

--- src/modules/discount/supplier/discount.supplier.module.ts
+++ src/modules/discount/supplier/discount.supplier.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DiscountEntity } from '../entites/discount.entity';
+import { DiscountSupplierController } from './discount.supplier.controller';
+import { DiscountSupplierService } from './discount.supplier.service';
+import { SupplierService } from 'src/modules/supplier/supplier.service';
+import { SupplierEntity } from 'src/modules/supplier/entities/supplier.entity';
+import { CategoryAppService } from 'src/modules/category/client/category.client.service';
+import { CategoryEntity } from 'src/modules/category/entities/category.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forFeature([DiscountEntity, SupplierEntity, CategoryEntity]),
+  ],
+  controllers: [DiscountSupplierController],
+  providers: [
+    DiscountSupplierService,
+    SupplierService,
+    CategoryAppService,
+    JwtService,
+  ],
 })
 export class DiscountSupplierModule {}

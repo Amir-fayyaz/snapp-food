e9a9469fc126158359c041c4beac085fcef02454
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-03 10:06:35 -0700

    add getSupplierDiscounts api

---
+++ src/modules/discount/supplier/discount.supplier.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from '@nestjs/common';
+import { DiscountSupplierService } from './discount.supplier.service';
+import { SupplierAuth } from 'src/common/decorators/auth.decorator';
+import { Role } from 'src/common/decorators/role.decorator';
+import { getUser } from 'src/common/decorators/getusers.decorator';
+import { ISupplier } from 'src/common/types/request-user.types';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@Controller('api/v1/supplier/discount')
+@SupplierAuth()
+@ApiTags('supplier-discount')
+export class DiscountSupplierController {
+  constructor(private readonly discountService: DiscountSupplierService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'For get discounts of special discount' })
+  @Role(['supplier'])
+  async getSupplierDiscounts(@getUser() supplier: ISupplier) {
+    return await this.discountService.getDiscountsForSupplier(
+      supplier.supplier_id,
+    );
+  }
+}

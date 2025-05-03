7143a75315415b682f49ee45755b629e06bcd9de
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-03 14:07:43 -0700

    add deleteDiscount by code api

--- src/modules/discount/supplier/discount.supplier.controller.ts
+++ src/modules/discount/supplier/discount.supplier.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { DiscountSupplierService } from './discount.supplier.service';
 import { SupplierAuth } from 'src/common/decorators/auth.decorator';
 import { Role } from 'src/common/decorators/role.decorator';
 import { getUser } from 'src/common/decorators/getusers.decorator';
 import { ISupplier } from 'src/common/types/request-user.types';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { CreateDiscountDto } from './dto/create-discount.dto';
 
 @Controller('api/v1/supplier/discount')
@@ -13,6 +13,7 @@ import { CreateDiscountDto } from './dto/create-discount.dto';
 export class DiscountSupplierController {
   constructor(private readonly discountService: DiscountSupplierService) {}
 
+  //GET
   @Get()
   @ApiOperation({ summary: 'For get discounts of special discount' })
   @Role(['supplier'])
@@ -22,6 +23,7 @@ export class DiscountSupplierController {
     );
   }
 
+  //POST
   @Post()
   @ApiOperation({ summary: 'For create discount by supplier' })
   @ApiBody({ type: CreateDiscountDto })
@@ -31,6 +33,24 @@ export class DiscountSupplierController {
   ) {
     return await this.discountService.createDiscount(
       data,
+      supplier.supplier_id,
+    );
+  }
+
+  //DELETE
+  @Delete(':code')
+  @ApiOperation({ summary: 'For delete discount-coupon by code' })
+  @ApiParam({
+    name: 'code',
+    type: String,
+    description: 'code of discount-coupon',
+  })
+  async deleteDiscount(
+    @Param('code') code: string,
+    @getUser() supplier: ISupplier,
+  ) {
+    return await this.discountService.deleteDiscount(
+      code,
       supplier.supplier_id,
     );
   }

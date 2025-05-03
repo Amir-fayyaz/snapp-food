e75eccd065afc34ce0767e053c7b3cf9d6ece4ce
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-03 11:24:54 -0700

    add create-discount api for supplier

--- src/modules/discount/supplier/discount.supplier.controller.ts
+++ src/modules/discount/supplier/discount.supplier.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { DiscountSupplierService } from './discount.supplier.service';
 import { SupplierAuth } from 'src/common/decorators/auth.decorator';
 import { Role } from 'src/common/decorators/role.decorator';
 import { getUser } from 'src/common/decorators/getusers.decorator';
 import { ISupplier } from 'src/common/types/request-user.types';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateDiscountDto } from './dto/create-discount.dto';
 
 @Controller('api/v1/supplier/discount')
 @SupplierAuth()
@@ -17,6 +18,19 @@ export class DiscountSupplierController {
   @Role(['supplier'])
   async getSupplierDiscounts(@getUser() supplier: ISupplier) {
     return await this.discountService.getDiscountsForSupplier(
+      supplier.supplier_id,
+    );
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'For create discount by supplier' })
+  @ApiBody({ type: CreateDiscountDto })
+  async createDiscount(
+    @Body() data: CreateDiscountDto,
+    @getUser() supplier: ISupplier,
+  ) {
+    return await this.discountService.createDiscount(
+      data,
       supplier.supplier_id,
     );
   }

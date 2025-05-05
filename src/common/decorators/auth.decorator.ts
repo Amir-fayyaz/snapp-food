c4a0c323d953ed6d6395bdb7bc07264e248218e0
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-05 00:01:41 -0700

    add userGuard to UserAuth decorator

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -2,12 +2,13 @@ import { applyDecorators, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiHeader } from '@nestjs/swagger';
 import { SupplierGuard } from 'src/modules/auth/guards/supplier.guard';
 import { RoleGuard } from '../guards/role.guard';
+import { UserGuard } from 'src/modules/auth/guards/user.guard';
 
 export function UserAuth() {
   return applyDecorators(
     ApiBearerAuth('Authorizaion'),
     ApiHeader({ name: 'Authorization', required: true }),
-    UseGuards(RoleGuard),
+    UseGuards(RoleGuard, UserGuard),
   );
 }
 

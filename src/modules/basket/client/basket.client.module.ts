0064868c98e3f3e3db892b4789efbb3c22c6b500
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-07 15:37:48 -0700

    add dependencies for basket-app module

--- src/modules/basket/client/basket.client.module.ts
+++ src/modules/basket/client/basket.client.module.ts
@@ -8,10 +8,25 @@ import { MenuEntity } from 'src/modules/menu/entities/menu.entity';
 import { JwtService } from '@nestjs/jwt';
 import { UserAppService } from 'src/modules/users/client/user.client.service';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { DiscountAppService } from 'src/modules/discount/client/discount.client.service';
+import { DiscountEntity } from 'src/modules/discount/entites/discount.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BasketEntity, MenuEntity, UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      BasketEntity,
+      MenuEntity,
+      UserEntity,
+      DiscountEntity,
+    ]),
+  ],
   controllers: [BasketAppController],
-  providers: [BasketAppService, MenuAppService, JwtService, UserAppService],
+  providers: [
+    BasketAppService,
+    MenuAppService,
+    JwtService,
+    UserAppService,
+    DiscountAppService,
+  ],
 })
 export class BasketAppModule {}

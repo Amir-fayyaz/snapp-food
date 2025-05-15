bcc805e509c15e3269747413abd2a2343e9b53a2
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-15 12:31:08 -0700

    add getSocketClient decorator

--- src/common/decorators/getusers.decorator.ts
+++ src/common/decorators/getusers.decorator.ts
@@ -1,17 +1,14 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Socket } from 'socket.io';
 
 export const getUser = createParamDecorator(
   (data: string, context: ExecutionContext) => {
-    // if (data === 'user') {
     return context.switchToHttp().getRequest().user;
-    // }
+  },
+);
 
-    // if (data === 'admin') {
-    //   return context.switchToHttp().getRequest().admin;
-    // }
-
-    // if (data === 'supplier') {
-    //   return context.switchToHttp().getRequest().supplier;
-    // }
+export const getSocketClient = createParamDecorator(
+  (data: string, context: ExecutionContext) => {
+    return context.switchToWs().getClient<Socket>().user;
   },
 );

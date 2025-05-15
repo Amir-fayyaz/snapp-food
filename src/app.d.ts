0ef8aa5c06543323f21bd19814168931ab6f330d
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-15 12:03:47 -0700

    add Socket-interface

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,4 @@
+import { ISupplier, IUser } from './common/types/request-user.types';
 import { UserEntity } from './modules/users/entities/user.entity';
 
 declare global {
@@ -7,5 +8,13 @@ declare global {
       supplier?: any;
       admin: any;
     }
+  }
+}
+
+import { Socket } from 'socket.io';
+
+declare module 'socket.io' {
+  interface Socket {
+    user?: IUser;
   }
 }
